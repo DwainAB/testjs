79940d327cf3dc5943bdd5a706cc8e9daf412e59
Dwain <dwaincontact@gmail.com>
2023-10-23 17:26:44 +0200

    exo 3

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let btn = document.querySelector("button")
 let img = document.querySelector("img")
 let select = document.querySelector("select")
+let containerListImg = document.querySelector('.container-list-img')
 let response
 let pictureDog
+let imgSave = []
 
 async function getDog() {
 
@@ -10,15 +12,22 @@ async function getDog() {
         response = await fetch("https://dog.ceo/api/breeds/image/random");
         pictureDog = await response.json();
         img.src = pictureDog.message
-        console.log('ttt');
     }else{
         response = await fetch(`https://dog.ceo/api/breed/${select.value}/images/random`);
         pictureDog = await response.json();
         img.src = pictureDog.message
-        console.log("test");
     }
 
 }
 
+
 btn.addEventListener("click", getDog)
+
+img.addEventListener('click', () =>{
+    imgSave.push(img.src)
+    let createImg  = document.createElement('img')
+    createImg.src = img.src
+    containerListImg.appendChild(createImg)
+
+})
 
