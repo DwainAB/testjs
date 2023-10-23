660925358ad56a4e1825dc54032ee8b34a6437a4
Dwain <dwaincontact@gmail.com>
2023-10-23 16:52:39 +0200

    exo 2

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 let btn = document.querySelector("button")
 let img = document.querySelector("img")
+let select = document.querySelector("select")
+let response
+let pictureDog
 
 async function getDog() {
-    const reponse = await fetch("https://dog.ceo/api/breeds/image/random");
-    const pictureDog = await reponse.json();
-    img.src = pictureDog.message
+
+    if(select.value == "/"){
+        response = await fetch("https://dog.ceo/api/breeds/image/random");
+        pictureDog = await response.json();
+        img.src = pictureDog.message
+        console.log('ttt');
+    }else{
+        response = await fetch(`https://dog.ceo/api/breed/${select.value}/images/random`);
+        pictureDog = await response.json();
+        img.src = pictureDog.message
+        console.log("test");
+    }
+
 }
 
 btn.addEventListener("click", getDog)
